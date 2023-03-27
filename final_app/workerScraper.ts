5037724d0fa7f199334b4e9a26ccd1d99a8c96b7
Max Shulov <theipicon@gmail.com>
2023-03-27 11:32:26 +0300

    converted to promse all

--- final_app/workerScraper.ts
+++ final_app/workerScraper.ts
@@ -20,18 +20,25 @@ const workerScraper = async () => {
     distinct: 'symbol'
   });
 
-  for (const { symbol } of symbols) {
-    const res = (
-      await axios.get(`https://www.google.com/finance/quote/${symbol}-USD`)
-    ).data;
+  const sitePromise = symbols.map(({ symbol }) =>
+    axios.get(`https://www.google.com/finance/quote/${symbol}-USD`)
+  );
 
-    const $ = cheerio.load(res);
+  const responses = await Promise.all(sitePromise);
+
+  for (
+    let responseIndex = 0;
+    responseIndex < responses.length;
+    responseIndex++
+  ) {
+    const $ = cheerio.load(responses[responseIndex].data);
     const value = $("[jsname='LXPcOd'] [jsname='ip75Cb']")
       .text()
       .replace(',', '');
+    const { symbol } = symbols[responseIndex];
 
     const newSymbol = new Symbol({
-      symbol: 'BTC',
+      symbol: symbol,
       value: parseFloat(value),
       scrapedAt: new Date()
     });
